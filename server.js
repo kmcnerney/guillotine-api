bd8d5db4b6b179bc1255e027954d6ef5bfb219e7
Kevin McNerney <kevin.m.mcnerney@gmail.com>
2023-09-28 15:34:54 -0400

    login to yahoo with selenium

---
+++ server.js
@@ -0,0 +1,37 @@
+const { Builder, By, Key, until } = require('selenium-webdriver');
+
+async function getLiveProjections() {
+  const driver = new Builder()
+    .forBrowser('chrome')
+    .build();
+
+  try {
+    // Navigate to the Yahoo Fantasy login page
+    await driver.get('https://login.yahoo.com');
+
+    // Find the username input field and enter your Yahoo username/email
+    await driver.findElement(By.id('login-username')).sendKeys('your_username');
+
+    // Find and click the "Next" button (Yahoo sometimes requires two steps for login)
+    await driver.findElement(By.id('login-signin')).click();
+
+    // Wait for the password input field to become visible
+    await driver.wait(until.elementLocated(By.id('login-passwd')), 5000);
+
+    // Find the password input field and enter your password
+    await driver.findElement(By.id('login-passwd')).sendKeys('your_password');
+
+    // Find and click the "Sign In" button
+    await driver.findElement(By.id('login-signin')).click();
+
+    // Optional: Add code to handle post-login actions on the Yahoo Fantasy site
+    // ...
+
+    console.log('Login successful.');
+  } finally {
+    // Close the browser window
+    await driver.quit();
+  }
+}
+
+getLiveProjections();
