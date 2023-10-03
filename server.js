aaec9c5fa467e7826621c4c38430cf4ee07c7ea0
Kevin McNerney <kevin.m.mcnerney@gmail.com>
2023-10-02 23:00:08 -0400

    log the full page body

--- server.js
+++ server.js
@@ -48,47 +48,48 @@ async function login() {
   }
 }
 
-const _scraper = () => {
-  console.log('running script')
-  const weeklySection = document.getElementById('matchupweek')
-  const leagueTable = weeklySection.getElementsByClassName('Table')
-  const leagueTableBody = leagueTable[0].getElementsByTagName('tbody')
-  const teams = leagueTableBody[0].getElementsByTagName('tr')
+// const _scraper = () => {
+//   console.log('running script')
+//   const weeklySection = document.getElementById('matchupweek')
+//   const leagueTable = weeklySection.getElementsByClassName('Table')
+//   const leagueTableBody = leagueTable[0].getElementsByTagName('tbody')
+//   const teams = leagueTableBody[0].getElementsByTagName('tr')
 
-  let scores = []
-  for(const team of teams) {
-    const cells = team.getElementsByTagName('td')
-    const teamName = cells[2].getElementsByTagName('a')[0].innerHTML
-    const projScore = cells[4].innerHTML
-    scores.push({
-      teamName: teamName,
-      projectedPts: projScore
-    })
-  }
-  return scores
-}
+//   let scores = []
+//   for(const team of teams) {
+//     const cells = team.getElementsByTagName('td')
+//     const teamName = cells[2].getElementsByTagName('a')[0].innerHTML
+//     const projScore = cells[4].innerHTML
+//     scores.push({
+//       teamName: teamName,
+//       projectedPts: projScore
+//     })
+//   }
+//   return scores
+// }
 
 async function getLiveProjections() {
   let scores = []
   try {
-    scores = await driver.executeScript(_scraper)
+    // scores = await driver.executeScript(_scraper)
 
-    // await driver.wait(until.elementLocated(By.id('matchupweek')), 5000)
-    // const weeklySection = await driver.findElement(By.id('matchupweek'))
-    // const leagueTable = await weeklySection.findElements(By.className('Table'))
-    // const leagueTableBody = await leagueTable[0].findElements(By.tagName('tbody'))
-    // const teams = await leagueTableBody[0].findElements(By.tagName('tr'))
+    const pageSource = await driver.wait(until.elementLocated(By.tagName('body')), 5000).getAttribute('innerHTML');
+    console.log('pageSource: ', pageSource);
 
-    // for(const team of teams) {
-    //   const cells = await team.findElements(By.tagName('td'))
-    //   const teamCell = await cells[2].findElement(By.tagName('a'))
-    //   const teamName = await teamCell.getAttribute('innerHTML')
-    //   const projScore = await cells[3].getAttribute('innerHTML')
-    //   scores.push({
-    //     teamName: teamName,
-    //     projectedPts: projScore
-    //   })
-    // }
+    await driver.wait(until.elementLocated(By.id('matchupweek')), 5000)
+    const weeklySection = await driver.findElement(By.id('matchupweek'))
+    const leagueTable = await weeklySection.findElements(By.className('Table'))
+    const leagueTableBody = await leagueTable[0].findElements(By.tagName('tbody'))
+    const teams = await leagueTableBody[0].findElements(By.tagName('tr'))
+
+    for(const team of teams) {
+      const cells = await team.findElements(By.tagName('td'))
+      const teamCell = await cells[2].findElements(By.tagName('a'))
+      scores.push({
+        teamName: await teamCell[0].getAttribute('innerHTML'),
+        projectedPts: await cells[3].getAttribute('innerHTML')
+      })
+    }
   } catch (e) {
     console.error('Failed to get live projections from Yahoo', e)
     await driver.quit()
