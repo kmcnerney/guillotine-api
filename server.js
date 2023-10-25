3750639b7233143cf7b8decd242a58caa6c87021
Kevin McNerney <kevin.m.mcnerney@gmail.com>
2023-10-24 20:57:53 -0400

    try with a wait before clicking

--- server.js
+++ server.js
@@ -74,9 +74,10 @@ async function getLiveProjections() {
     const pageSource = await driver.getPageSource();
     if(pageSource.includes('Final results')) {
       console.log('Skipping to next week projections');
+      await new Promise(r => setTimeout(r, 500))
       const nextButton = await driver.findElements(By.className('Js-next'));
       await nextButton[0].click();
-      await new Promise(r => setTimeout(r, 1000))
+      await new Promise(r => setTimeout(r, 500))
     }
 
     const weeklySection = await driver.findElement(By.id('matchupweek'))
