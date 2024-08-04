3c828dea72e9c726c07310cda23f15cef54b58e3
Kevin McNerney <kevin.mcnerney@gethawk.com>
2024-08-04 12:21:06 -0400

    more logging

--- server.js
+++ server.js
@@ -73,7 +73,9 @@ async function getLiveProjections() {
   try {
     console.log('refreshing page')
     await driver.navigate().refresh()
+    console.log('refreshed')
     await new Promise((r) => setTimeout(r, 1000))
+    console.log('making sure the page is ready')
     await driver.wait(until.elementLocated(By.id('matchupweek')), 5000)
   } catch (e) {
     console.error('failed to find league page, need to re-login')
